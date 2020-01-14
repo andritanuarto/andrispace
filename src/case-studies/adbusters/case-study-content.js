5eede4fe620dc0fc597cb9902034dc94ce74f5d5
Andri Tanuarto <andritanuarto@gmail.com>
2020-01-14 15:16:50 -0800

    Continue Adbusters case study

--- src/case-studies/adbusters/case-study-content.js
+++ src/case-studies/adbusters/case-study-content.js
@@ -8,7 +8,13 @@ const caseStudyContent = (
     <div className="par par--normal-width">
       <p>
         <h2>Client Background</h2>
-        When I studied at <a href="https://redacademy.com/vancouver/" target="_blank">RED Academy Vancouver</a>, my team was given a great opportunity to work with <a href="https://www.adbusters.org/" target="_blank">Adbusters</a> for our client project. Adbusters is a Canadian-based publishing magazine organization with a strong presence in the North American market for a political art magazine. Anti-capitalism, subvertisements, environmental and political movements are some of their typical content in their magazine.
+        When I studied at <a href="https://redacademy.com/vancouver/" target="_blank">RED Academy Vancouver</a>, my team was given a great opportunity to work with <a href="https://www.adbusters.org/" target="_blank">Adbusters</a> for our client project.
+      </p>
+      <p>
+        Our client is a Canadian-based publishing company. Their magazine has a strong presence in the North American market. Adbusters describes itself as "a global network of artists, activists, writers, pranksters, students, educators and entrepreneurs who want to advance the new social activist movement of the information age."
+      </p>
+      <p>
+        Topics related to anti-capitalism, subvertisements, pro-environment and political movements are some of their typical content in their magazine.
       </p>
       <p>
       What's unique about Adbusters magazine is the unusual style compare to other magazines. Their approach of anti-big commercial means consistency of their content is almost non-existent unlike what most commercial companies do. Their content style tent to be free-flowing and spontaneous.
@@ -27,7 +33,7 @@ const caseStudyContent = (
         />
       </p>
       <p>
-        Besides publishing magazines, Adbusters also actively organizes and involves different social campaigns like <a href="https://en.wikipedia.org/wiki/Culture_jamming" target="_blank">Culture Jamming</a>, <a href="http://abillionpeople.org/8th-anniversary-of-occupywallstreet/" target="_blank">Occupy Wall Street</a> and <a href="http://abillionpeople.org/buy-nothing-day-2019/">Buy Nothing Day</a>.
+        Besides publishing magazines, Adbusters also actively organizes and involves different social campaigns like <a href="https://en.wikipedia.org/wiki/Culture_jamming" target="_blank">Culture Jamming</a>, <a href="http://abillionpeople.org/8th-anniversary-of-occupywallstreet/" target="_blank">Occupy Wall Street</a> and <a href="http://abillionpeople.org/buy-nothing-day-2019/">, etc</a>.
       </p>
       <p>
         <h2>Project Brief</h2>
@@ -38,6 +44,10 @@ const caseStudyContent = (
       </p>
       <p>
         Our initial thought this project is going be interesting, especially about the art direction that would be a lot of room to do experimentation, but we also aware of the necessity to find a good balance between Adbusters' original style without sacrificing the usability of the app.
+      </p>
+      <p>
+        <h3>My Role</h3>
+        There are 4 people were working on this project. I involved in every phase of the UX processes such as research, planning, and design, and setting up the standard library following the atomic design principle. Because of my varied background as a software UI designer and developer, I also helped some of the UI work for this project and advising the feasibility of translating design into real-life development.
       </p>
     </div>
   </>
