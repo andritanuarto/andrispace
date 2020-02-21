0aa8f45e8f6fa6e4cd7beb30deb353a8b6fc7bc5
Andri Tanuarto <andritanuarto@gmail.com>
2020-02-21 13:27:56 -0800

    Add social media

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,8 @@ import InteriorContainer from '../components/shared/interior-container';
 import instagram from '../img/instagram.svg';
 import twitter from '../img/twitter.svg';
 import github from '../img/github-cat.svg';
+import linkedin from '../img/linkedin.svg';
+import SVG from 'react-inlinesvg';
 
 export default () => (
   <>
@@ -12,19 +14,20 @@ export default () => (
       <div className="interior-page__content">
         <div className="contact">
           <div className="contact__left-side">
-            <h3>Contact</h3>
             <strong>Andri Tanuarto</strong>
             <span className="contact__email-detail">
               e-mail: <a href="mailto:andritanuarto@gmaio.com">andritanuarto@gmail.com</a>
             </span>
-            <ul>
-              <li><a href="#">{instagram}</a></li>
-              <li><a href="#">{twitter}</a></li>
-              <li><a href="#">{github}</a></li>
+            <ul className="contact__social-media-links">
+              <li><a target="_blank" href="http://instagram.com/andritanuarto"><SVG src={instagram} /></a></li>
+              <li><a target="_blank" href="https://twitter.com/andritanuarto" href="#"><SVG src={twitter} /></a></li>
+              <li><a target="_blank" href="https://github.com/andritanuarto"><SVG src={github} /></a></li>
+              <li><a target="_blank" href="https://www.linkedin.com/in/andritanuarto/"><SVG src={linkedin} /></a></li>
             </ul>
           </div>
           <div className="contact__right-side">
             <form className="contact__form" name="contact" method="POST" data-netlify="true">
+              <h3>Send a message</h3>
               <p>
                 <label>
                   Full Name <span className="required">*</span>
