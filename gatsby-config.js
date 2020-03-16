b7caa1e52981ec07d8105b86ce6bcbaaed182a40
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-16 11:58:30 -0700

    Change favicon setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        icon: `./static/as-favicon.ico`
+        icon: `./static/fav.png`
       }
     },
     {
