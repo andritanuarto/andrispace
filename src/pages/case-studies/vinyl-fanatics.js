a6feb929a5822906bb0c3399816f9e489815da91
Andri Tanuarto <andritanuarto@gmail.com>
2020-03-23 16:32:16 -0700

    Continue Vinyl Fanatics case study

--- src/pages/case-studies/vinyl-fanatics.js
+++ src/pages/case-studies/vinyl-fanatics.js
@@ -5,9 +5,9 @@ import caseStudyBanner from '../../img/vinyl-fanatics/case-study-banner.jpeg';
 export const frontmatter = {
   clientName: 'Vinyl Fanatics',
   projectDate: '2019-05-15',
-  status: 'draft',
+  status: 'published',
   projectTitle: 'Vinyl Fanatics – An online store for vinyl collectors',
-  postTitle: 'An online store for Vinyl Fanatics',
+  postTitle: 'An online store for vinyl collectors',
   blurb: 'Buy records online to grow your music collection',
   logoWidth: 'auto',
   opacityColor: '#000000',
@@ -28,10 +28,18 @@ export default () => (
     <>
       <div className="par par--normal-width">
         <p className="first-paragraph">
-          Way before mp3, cd even cassette, the vinyl records enjoyed the popularity among music lovers. It was the primary medium for music reproduction in the 20th century. Although the industry faced stiff competition when the compact cassettes were mass-produced in the 1910s, it still able maintained the largest market share.
+          Before mp3, cd or even cassette, the vinyl records industry enjoyed its renaissance among music lovers. It was the primary medium for music reproduction in the 20th century. Although the industry faced stiff competition when the compact cassettes were mass-produced in the 1910s, it still able maintained the largest market share. By the early 90s, the CD became the most preferred medium for people to collect music. As CD continued to grow, it contributed to a major decline in vinyl sales.
         </p>
         <p>
-          Records industry experienced a major decline when CD (Compact Disc) was released in the early 80s, and it becomes the mainstream for the younger generation in the 90s.
+          After declining market in the 90s to early 2000s, vinyl records experienced a resurgence in 2008. Its sales reached 1.9 million units, and the increasing trend continues and eventually outsell CDs in 2019 for the first time in 40 years. Today vinyl records established its niche besides the popularity of online music platforms. It attracted not only older generations, but more millennials started to enjoy vinyl records as an alternative way to enjoy music.
+        </p>
+        <p>
+          <h2>Project Brief</h2>
+          Started as a small store in Singapore, they wanted to sell their products beyond the local market through the e-commerce website. The new website aims to attract more buyers and becoming more relevant for the increasing number of younger audiences to enjoy music using vinyl records. Their website will be developed using Shopify and targeting for both mobile and desktop users.
+        </p>
+        <p>
+          <h3>The Challenge</h3>
+          A lot of people enjoy going to the physical store and do crate surfing to buy vinyl, but not everyone has the access and time to go to the physical store. The challenge is how to accommodate and to create a good experience for people to browse and purchase vinyl online, and also attracting younger audiences.
         </p>
       </div>
     </>
