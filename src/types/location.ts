8ac473fd99f5b770dc0ee11506d3957aef19e393
Andri Tanuarto <andritanuarto@gmail.com>
2021-10-31 21:00:41 -0700

    Add typescript library

---
+++ src/types/location.ts
@@ -0,0 +1,15 @@
+export type locationType = {
+  location: {
+    hash: string;
+    host: string;
+    hostname: string;
+    href: string;
+    key: string;
+    origin: string;
+    pathname: string;
+    port: string;
+    protocol: string;
+    search: string;
+    state: null
+  }; 
+};
